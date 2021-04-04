c74235bd226cb699bac99b070a8d7201ed320fac
johnckealy <johnckealy@gmail.com>
2021-04-04 18:24:47 +0100

    Fix for dir struction in vagrant caddy set up script

--- .djengu/.production_toolbox/caddy/vagrant_caddy.sh
+++ .djengu/.production_toolbox/caddy/vagrant_caddy.sh
@@ -10,7 +10,7 @@ export API_DOMAIN=`grep -P "API_URL" env/.env.prod | sed -e "s/^API_URL=https:\/
 export ORIGIN_DOMAIN=`grep -P "ORIGIN_URL" env/.env.prod | sed -e "s/^ORIGIN_URL=https:\/\///"`
 envsubst < "./.djengu/.production_toolbox/caddy/Caddyfile.template" > "./.djengu/.production_toolbox/caddy/Caddyfile"
 vagrant ssh -c "cd /caddy/
-                cp /djengu/.djengu/.production_toolbox/caddy/Caddyfile /caddy/Caddyfile
+                cp /djengu/.djengu/.production_toolbox/caddy/Caddyfile /caddy/
                 docker-compose -f docker-compose.caddy.yml down -v && \
                 docker-compose -f docker-compose.caddy.yml up --build -d"
 
