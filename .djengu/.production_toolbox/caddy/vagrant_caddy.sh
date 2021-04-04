5131919d8a783dff9317c8c4d5ea47a34eb11d19
johnckealy <johnckealy@gmail.com>
2021-04-04 15:21:31 +0100

    update the vagrant_caddy setup file

--- .djengu/.production_toolbox/caddy/vagrant_caddy.sh
+++ .djengu/.production_toolbox/caddy/vagrant_caddy.sh
@@ -1,4 +1,10 @@
-# Create the Caddyfile
+#!/bin/bash
+set -e
+
+GREEN='\033[0;32m'
+
+echo -e "\n${GREEN}Configuring vagrant. This may take a few minutes...${NC}\n"
+vagrant up
 export HOST=`hostname -I | grep -oP "192+[.]\d+[.]\d+[.]\d+"`
 export API_DOMAIN=`grep -P "API_URL" env/.env.prod | sed -e "s/^API_URL=https:\/\///"`
 export ORIGIN_DOMAIN=`grep -P "ORIGIN_URL" env/.env.prod | sed -e "s/^ORIGIN_URL=https:\/\///"`
@@ -7,3 +13,7 @@ vagrant ssh -c "cd /caddy/
                 cp /djengu/.djengu/.production_toolbox/caddy/Caddyfile /caddy/Caddyfile
                 docker-compose -f docker-compose.caddy.yml down -v && \
                 docker-compose -f docker-compose.caddy.yml up --build -d"
+
+echo -e "\n${GREEN}🚀 Vagrant is up and running. You can enter the instance using:\n"
+echo "vagrant ssh"
+echo
